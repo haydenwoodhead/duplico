374c91a37e6a815e4871d1de66f0855cd6002f4e
Hayden Woodhead <hayden@movio.co>
2020-05-24 19:05:59 +1200

    Add initial graphql stuff

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,29 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/haydenwoodhead/duplico/graph/generated"
+	"github.com/haydenwoodhead/duplico/graph/model"
+)
+
+func (r *mutationResolver) RegisterUser(ctx context.Context) (*model.RegisterUserOutput, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Ping(ctx context.Context) (*model.Pong, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
